0a0dc7930ddde5ee16c248f5a134478dd7b4bdcc
bvbishnoi33-png <bvbishnoi33@gmail.com>
2026-01-02 17:31:21 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -2,8 +2,15 @@ import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/fi
 
 onAuthStateChanged(auth, user => {
   if (!user) {
-    alert("Login required");
-    window.location.href = "/";
+    import { onAuthStateChanged } 
+from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "login.html";
+  }
+});
+
   }
 });
 
